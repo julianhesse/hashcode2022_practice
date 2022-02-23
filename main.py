723b417edebb7f41dd81ca8f0dd978a329d17b7c
Julian Hesse <julian.hesse@fu-berlin.de>
2022-02-23 01:11:48 +0100

    fixes spelling error of instance

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from solver_base import Solver_Base
 
 def main():
 
-    for fname in ["a_an_example.in", "b_basic.in", "c_coarse.in", "d_difficult", "e_elaborate.in"]:
+    for fname in ["a_an_example.in", "b_basic.in", "c_coarse.in", "d_difficult.in", "e_elaborate.in"]:
         Solver_Base(name=fname).compute_solution()
 
 if __name__ == "__main__":
