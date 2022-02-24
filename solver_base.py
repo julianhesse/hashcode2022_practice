17823a464b1374de42c8c9a578fa80b2e3fdcd27
Julian Hesse <julian.hesse@fu-berlin.de>
2022-02-24 02:11:12 +0100

    adds new solver method using pandas

--- solver_base.py
+++ solver_base.py
@@ -1,6 +1,7 @@
 from util import prep_dir
 import itertools
 import numpy as np
+import pandas as pd
 
 
 class Solver_Base:
@@ -115,6 +116,97 @@ class Solver_Base:
                 self.ingredients.pop()
                 score_prev = score_new
 
+    
+    def pandas_solution(self):
+        liked_counts = self.count_ing_list(self.ing_liked)
+        disliked_counts = self.count_ing_list(self.ing_disliked)
+        self.ingredients = list(liked_counts.keys())
+
+        df = pd.DataFrame({'liked': list(self.ing_liked), 'disliked': list(self.ing_disliked)})
+
+        like_scores = []
+        dislike_scores = []
+        for ing_liked, ing_disliked in zip(self.ing_liked, self.ing_disliked):
+            # calc a score for liked ingredients by how frequent the ingredients are
+            like_score = sum([liked_counts[ing] for ing in ing_liked])
+            like_scores.append(like_score)
+
+            # calc a score for disliked ingredients by how frequent the ingredients are
+            dislike_score = sum([disliked_counts[ing] for ing in ing_disliked])
+            dislike_scores.append(dislike_score)
+
+        df['like score'] = like_scores
+        df['avg like score'] = list(map(lambda x, y: (x/len(y)) if x else 0, df['like score'], df['liked']))
+        df['dislike score'] = dislike_scores
+        df['avg dislike score'] = list(map(lambda x, y: (x/len(y)) if x else 0, df['dislike score'], df['disliked']))
+
+        print(df)
+
+        # df = df.sort_values(by=['like score', 'dislike score'], ascending=[False, True])
+        df = df.sort_values(by=['avg like score', 'avg dislike score'], ascending=[False, True])
+        # df = df.sort_values(by=['dislike score', 'like score'], ascending=[True, False])
+        # df = df.sort_values(by=['avg dislike score', 'avg like score'], ascending=[True, False])
+        print(df)
+
+        prev_score = self.overall_score()
+        for i in df.index:
+            ingredients = set(self.ingredients) # optimization
+            # find out if removing disliked ingredients is increasing the score
+            disliked_ing = []
+            for ing in df.iloc[i]['disliked']:
+                if ing in ingredients:
+                    disliked_ing.append(ing)
+
+            for ing in disliked_ing:
+                self.ingredients.remove(ing)
+
+            new_score = self.overall_score()
+            if prev_score > new_score:
+                self.ingredients.extend(disliked_ing)
+            else:
+                prev_score = new_score
+
+        # prev_score = self.overall_score()
+        # for i in df.index:
+        #     ingredients = set(self.ingredients) # optimization
+        #     # find out if adding disliked ingredients are increasing the score
+        #     disliked_ing = []
+        #     for ing in df.iloc[i]['disliked']:
+        #         if ing not in ingredients:
+        #             disliked_ing.append(ing)
+
+        #     self.ingredients.extend(ing)
+
+        #     new_score = self.overall_score()
+        #     if prev_score > new_score:
+        #         for ing in disliked_ing:
+        #             self.ingredients.pop()
+        #     else:
+        #         prev_score = new_score
+
+        for i in range(2):
+            removed = set(list(liked_counts.keys())).difference(set(self.ingredients))
+            score_prev = self.overall_score()
+            for ing in removed:
+                self.ingredients.append(ing)
+                score_new = self.overall_score()
+                if score_new < score_prev:
+                    self.ingredients.pop()
+                else:
+                    score_prev = score_new
+
+            # self.ingredients = list(set(self.ingredients))
+
+            for ing in self.ingredients:
+                self.ingredients.remove(ing)
+                score_new = self.overall_score()
+                if score_new < score_prev:
+                    self.ingredients.append(ing)
+                else:
+                    score_prev = score_new
+
+            self.ingredients = list(set(self.ingredients))
+
     # Remember: only save if new solution is better
     def save_solution(self, name=None):
         if name is None:
@@ -153,7 +245,7 @@ class Solver_Base:
 
 if __name__ == '__main__':
     files = ["a_an_example.in", "b_basic.in", "c_coarse.in", "d_difficult.in", "e_elaborate.in"]
-    test = Solver_Base(files[2])
+    test = Solver_Base(files[3])
     # print(test.count_ing_list(test.ing_disliked))
     # print(test.count_ing_list(test.ing_liked))
     print(len(test.count_ing_list(test.ing_disliked)))
@@ -161,6 +253,6 @@ if __name__ == '__main__':
     print(test.num_clients)
     # print(test.ing_liked)
     # print(test.ing_disliked)
-    test.simple_solution()
+    test.pandas_solution()
     print(test.overall_score())
     test.save_solution()
